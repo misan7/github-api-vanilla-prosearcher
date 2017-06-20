862ea634350c2fe42b4ebe55a8884f72e0caa10a
Sergio MT <sergio.mt88@gmail.com>
2017-06-20 16:51:09 +0200

    first ajax call with console.log and not found protection

---
+++ client/js/app.js
@@ -0,0 +1,29 @@
+var xhr
+document.getElementById("ajaxButton").addEventListener('click', makeRequest)
+
+function makeRequest() {
+
+    var xhr = new XMLHttpRequest();
+
+    username = document.getElementById("username").value
+    console.log("username", username);
+    var url = "https://api.github.com/users/" + username
+    console.log("url", url);
+
+    xhr.open('GET', url);
+    xhr.send(null);
+
+    xhr.onreadystatechange = function() {
+        var DONE = 4; // readyState 4 means the request is done.
+        var OK = 200; // status 200 is a successful return.
+        if (xhr.readyState === DONE) {
+            if (xhr.status === OK)
+                console.log(xhr.responseText); // 'This is the returned text.'
+              else if (xhr.status === 404) 
+                alert("The Profile you're searching for doesn't exist")
+        } else {
+            console.log('Error: ' + xhr.status); // An error occurred during the request.
+
+        }
+    }
+  }
